5b9ad23c7be21ccf40d83318c2f91f52feef5fb7
Ben Bennett <bb0139@my.bristol.ac.uk>
2020-07-08 17:44:34 +0200

    Implemented reverse, uniform, sum and max functions

---
+++ JavaScript/ArrayProblemSet/arrays.js
@@ -0,0 +1,46 @@
+let testNums = [1, 2, 3];
+let testItems = ["hello", "hello", "hello"];
+
+console.log(printReverse(testNums));
+console.log(isUniform(testNums));
+console.log(sumArray(testNums));
+console.log(max(testNums));
+
+
+function printReverse(numsToReverse) {
+    let numsReversed = [];
+    numsToReverse.forEach(function(number) {
+        numsReversed.unshift(number);
+    });
+
+    return numsReversed;
+}
+
+function isUniform(itemsToTest) {
+    let firstItem = itemsToTest[0];
+
+    itemsToTest.forEach(function(item) {
+        if (item !== firstItem) return false;
+    });
+    return true;
+}
+
+function sumArray(numbers) {
+    let total = 0;
+
+    numbers.forEach(function(value) {
+        total += value;
+    });
+
+    return total;
+}
+
+function max(numbers) {
+    let maxNum = numbers[0];
+
+    numbers.forEach(function(value) {
+        if (value >= maxNum) maxNum = value;
+    });
+
+    return maxNum;
+}
