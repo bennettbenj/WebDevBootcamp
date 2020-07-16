e4ad5de3173575aa8c5bd2867bc6d2a82d051cf0
Ben Bennett <bb0139@my.bristol.ac.uk>
2020-07-16 19:04:02 +0100

    More DOM exercises completed

---
+++ DOM/script.js
@@ -0,0 +1,7 @@
+let firsP = document.getElementById("first");
+
+let secondP = document.getElementsByClassName("special");
+
+let thirdP = document.querySelector("p");
+
+let fourthP = document.querySelectorAll("p")[3];
