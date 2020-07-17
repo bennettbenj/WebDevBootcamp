8d363c1e5af780926bd84bca3ab70f759757da31
Ben Bennett <bb0139@my.bristol.ac.uk>
2020-07-17 14:19:08 +0100

    Completed score keeper project

--- DOM/ColorChanger/script.js
+++ DOM/ColorChanger/script.js
@@ -1,7 +1,11 @@
+// let button = document.addEventListener("click", function() {
+//     if (document.body.style.background === "white") {
+//         document.body.style.background = "blue";
+//     } else {
+//         document.body.style.background = "white";
+//     }
+// });
+
 let button = document.addEventListener("click", function() {
-    if (document.body.style.background === "white") {
-        document.body.style.background = "blue";
-    } else {
-        document.body.style.background = "white";
-    }
+    document.body.classList.toggle("purple");
 });
---
+++ DOM/ScoreKeeperProject/scorekeeper.js
@@ -0,0 +1,50 @@
+let p1Button = document.querySelector("#p1");
+let p2Button = document.getElementById("p2");
+let p1Display = document.querySelector("#p1Display");
+let p2Display = document.querySelector("#p2Display");
+let resetButton = document.getElementById("reset");
+let numInput = document.querySelector("input");
+let paragraph = document.querySelector("p");
+let winningScoreDisplay = document.querySelector("p span");
+let p1Score = 0;
+let p2Score = 0;
+let gameOver = false;
+let winningScore = 5;
+
+p1Button.addEventListener("click", function() {
+    if (!gameOver) {
+        p1Score++;
+        if (p1Score === winningScore) {
+            p1Display.classList.add("winner");
+            gameOver = true;
+        }
+        p1Display.textContent = p1Score;
+    }
+});
+
+p2Button.addEventListener("click", function() {
+    if (!gameOver) {
+        p2Score++;
+        if (p2Score === winningScore) {
+            p2Display.classList.add("winner");
+            gameOver = true;
+        }
+        p2Display.textContent = p2Score;
+    }
+});
+
+resetButton.addEventListener("click", function() {
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = p1Score;
+    p2Display.textContent = p2Score;
+    p1Display.classList.remove("winner");
+    p2Display.classList.remove("winner");
+    gameOver = false;
+});
+
+numInput.addEventListener("change", function() {
+    paragraph.textContent = "Value changed"
+    winningScore = this.value;
+    winningScoreDisplay = this.value;
+})
