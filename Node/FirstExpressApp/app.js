753780eec3147000bb31c30904f3697e2794c6e7
Ben Bennett <bb0139@my.bristol.ac.uk>
2020-08-15 21:36:22 +0100

    Added more express routes

--- Node/FirstExpressApp/app.js
+++ Node/FirstExpressApp/app.js
@@ -2,7 +2,18 @@ let express = require("express");
 let app = express();
 
 app.get("/", function(req, res){
-    res.send("Hi there!");
+    res.send("Hi Steve you are now live!!!!");
 });
 
-app.listen(8080);
+app.get("/bye", function(req, res){
+    res.send("Goodbye");
+});
+
+app.get("/dog", function(req, res){
+    res.send("MEOW!");
+});
+
+app.listen(8080, function(){
+    console.log("Server has started!");
+});
+
